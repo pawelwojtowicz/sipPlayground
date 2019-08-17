d21f2f35a0fa2eba533c4ef97fa1dbbc54d20e03
Paweł Wójtowicz <elendil@konto.pl>
2019-08-17 21:47:39 +0200

    Call established between the phone and app

--- sipTestMain.cpp
+++ sipTestMain.cpp
@@ -3,6 +3,39 @@
 
 using namespace pj;
 
+class MyCall : public Call
+{
+public:
+    MyCall(Account &acc, int call_id = PJSUA_INVALID_ID)
+    : Call(acc, call_id)
+    { }
+
+    ~MyCall()
+    { }
+
+    // Notification when call's state has changed.
+    virtual void onCallState(OnCallStateParam &prm)
+    {
+    }
+
+    // Notification when call's media state has changed.
+    virtual void onCallMediaState(OnCallMediaStateParam &prm)
+    {
+      CallInfo ci = getInfo();
+      // Iterate all the call medias
+      for (unsigned i = 0; i < ci.media.size(); i++) {
+          if (ci.media[i].type==PJMEDIA_TYPE_AUDIO && getMedia(i)) {
+              AudioMedia *aud_med = (AudioMedia *)getMedia(i);
+
+              // Connect the call audio media to sound device
+              AudDevManager& mgr = Endpoint::instance().audDevManager();
+              aud_med->startTransmit(mgr.getPlaybackDevMedia());
+              mgr.getCaptureDevMedia().startTransmit(*aud_med);
+          }
+      }
+    }
+};
+
 
 // Subclass to extend the Account and get notifications etc.
 class MyAccount : public Account {
@@ -11,6 +44,14 @@ public:
         AccountInfo ai = getInfo();
         std::cout << (ai.regIsActive? "*** Register:" : "*** Unregister:")
                   << " code=" << prm.code << std::endl;
+    }
+
+    void onIncomingCall(OnIncomingCallParam &iprm)
+    {
+        Call *call = new MyCall(*this, iprm.callId);
+        CallOpParam prm;
+        prm.statusCode = PJSIP_SC_OK;
+        call->answer(prm);
     }
 };
 
@@ -41,9 +82,9 @@ int main(int argc, char** argv)
 
     // Configure an AccountConfig
     AccountConfig acfg;
-    acfg.idUri = "sip:6001@localhost";
+    acfg.idUri = "sip:5001@localhost";
     acfg.regConfig.registrarUri = "sip:localhost";
-    AuthCredInfo cred("asterisk", "*", "6001", 0, "unsecurepassword");
+    AuthCredInfo cred("asterisk", "*", "5001", 0, "pawel");
     acfg.sipConfig.authCreds.push_back( cred );
 
     // Create the account
@@ -51,7 +92,7 @@ int main(int argc, char** argv)
     acc->create(acfg);
 
     // Here we don't have anything else to do..
-    pj_thread_sleep(10000);
+    pj_thread_sleep(60000);
 
     // Delete the account. This will unregister from server
     delete acc;
